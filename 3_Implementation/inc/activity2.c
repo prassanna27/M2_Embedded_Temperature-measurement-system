3c8c9da9d0f6977c2ffb567f38d6d3a9246ec0fb
prassanna27 <94218817+prassanna27@users.noreply.github.com>
2021-12-01 19:26:51 +0530

    Create activity2.c

---
+++ 3_Implementation/inc/activity2.c
@@ -0,0 +1,41 @@
+/**
+ * @file activity2.h
+ * @author Pranav Pramod Jain
+ * @brief Reading values of temperature sensor using ADC
+ * @version 0.1
+ * @date 2021-12-01
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __ACTIVITY2_H__
+#define __ACTIVITY2_H__
+
+/**
+ * Include files
+ */ 
+#include "project_config.h"
+#include <avr/io.h>
+#include <util/delay.h>
+
+/**
+ * Macro Definitions
+ */
+#define TEMP_PIN 0
+
+/**
+ * @brief Reads the adc value from the given input channel
+ * 
+ * @param ch 
+ * @return Returns 10 bit value of ADC
+ */
+uint16_t read_adc(uint8_t ch);
+
+/**
+ * @brief Initialize all the Peripherals and pin configurations 
+ * 
+ */
+void activity2_init();
+
+#endif /** __ACTIVITY2_H__ */

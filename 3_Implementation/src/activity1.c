89a70fb0fcd3673d410a439e2878342f2dac0b49
prassanna27 <94218817+prassanna27@users.noreply.github.com>
2021-12-01 21:33:43 +0530

    Update activity1.c

--- 3_Implementation/src/activity1.c
+++ 3_Implementation/src/activity1.c
@@ -1,6 +1,6 @@
 #include<activity1.h>
-/* Function checks if button and heater is on */
-uint8_t button_heater_check(){
+/* Function checks if button and thermo is on */
+uint8_t button_thermo_check(){
 	return pin_read(BUTTON_STATUS, BUTTON_PIN) & pin_read(BUTTON_STATUS, HEATER_PIN);
 }
 
@@ -11,13 +11,13 @@ void activity1_init(){
 	/* Configure Button pin as Input */
     pin_mode(IN,BUTTON_DDR,BUTTON_PORT,BUTTON_PIN);
 	/* Configure Heater pin as Input */
-    pin_mode(IN,BUTTON_DDR,BUTTON_PORT,HEATER_PIN);
+    pin_mode(IN,BUTTON_DDR,BUTTON_PORT,THERMO_PIN);
 }
 
-/* Changes LED state based on button and heater */
+/* Changes LED state based on button and thermo */
 void change_led_state(){
     /* Check if both Switch is closed; i.e Pins 0 and 1 of port D is low*/
-    if(button_heater_check()){
+    if(button_thermo_check()){
         /* Turn on LED */
         pin_write(HIGH,LED_PORT,LED_PIN);
         _delay_ms(LED_ON_TIME);

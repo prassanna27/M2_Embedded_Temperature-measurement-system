549c207124d30c80c152c5bc4eb50d257d0fc682
prassanna27 <94218817+prassanna27@users.noreply.github.com>
2021-12-01 19:29:46 +0530

    Create activity4.c

---
+++ 3_Implementation/inc/activity4.c
@@ -0,0 +1,55 @@
+/**
+ * @file activity4.h
+ * @author  Pranav Pramod Jain
+ * @brief USART Protocol to transmit Temperature values
+ * @version 0.1
+ * @date 2021-12-01
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __ACTIVITY_4__
+#define __ACTIVITY_4__
+
+/**
+ * Include files
+ */ 
+#include "project_config.h"
+#include <avr/io.h>
+#include <util/delay.h>
+
+/**
+ * Macro definitions
+ */
+#define BAUD 9600 /**< Baud rate to 9600 */
+#define BAUDRATE (F_CPU/(BAUD*16UL)-1) /**< Baud rate conversion factor */
+
+/**
+ * @brief Initialize all the Peripherals and pin configurations
+ * 
+ */
+void activity4_init();
+
+/**
+ * @brief Writes a character on tx pin
+ * 
+ * @param ch 
+ */
+void write_ch_tx(char ch);
+
+/**
+ * @brief Reads a character on rx pin
+ * 
+ * @return Returns the read character 
+ */
+char read_ch_rx();
+
+/**
+ * @brief Writes a line on tx pin
+ * 
+ * @param str 
+ */
+void write_tx(char *str);
+
+#endif /** __ACTIVITY4_H__ */
